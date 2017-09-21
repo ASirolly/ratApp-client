64c907fed681a134c9b6c0bd14afc4d84440a910
jreno3 <joshuagregr@gmail.com>
2017-09-21 17:20:44 -0400

    added login and registration buttons

--- app/src/main/java/com/example/paramount/ratappandroid/MainActivity.java
+++ app/src/main/java/com/example/paramount/ratappandroid/MainActivity.java
@@ -1,13 +1,60 @@
 package com.example.paramount.ratappandroid;
 
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button login;
+    private Button registration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = (Button) findViewById(R.id.button_login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), Login.class);
+                startActivity(intent);
+            }
+        });
+
+        registration = (Button) findViewById(R.id.button_registration);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), Registration.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public Button getLoginButton() {
+        return login;
+    }
+
+    public Button getRegistrationButton() {
+        return registration;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.button_login) {
+            System.out.println("login + asdf");
+            navigateUpTo(new Intent(this, Login.class));
+            return true;
+        } else {
+            System.out.println("registration + asdf");
+            navigateUpTo(new Intent(this, Registration.class));
+        return super.onOptionsItemSelected(item);
+        }
     }
 }
---
+++ app/src/main/java/com/example/paramount/ratappandroid/Registration.java
@@ -0,0 +1,10 @@
+package com.example.paramount.ratappandroid;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by joshuareno on 9/21/17.
+ */
+
+public class Registration extends AppCompatActivity {
+}
