9f08157ea81460092508017cff704f0ca1c57f79
jreno3 <joshuagregr@gmail.com>
2017-09-22 11:15:17 -0400

    fixed access to login screen

--- app/src/main/java/com/example/paramount/ratappandroid/Login.java
+++ app/src/main/java/com/example/paramount/ratappandroid/Login.java
@@ -21,7 +21,7 @@ public class Login extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
 
         cancel = (Button) findViewById(R.id.cancelButtonLogin);
         cancel.setOnClickListener(new View.OnClickListener() {
