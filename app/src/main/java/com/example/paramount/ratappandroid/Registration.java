f24587c7c28b9370eae5ddf39190766b5e13c5f5
Greg <greg9381@gmail.com>
2017-09-21 22:34:41 -0400

    adding registration screen (submit button doesn't do anythng yet)

--- app/src/main/java/com/example/paramount/ratappandroid/Registration.java
+++ app/src/main/java/com/example/paramount/ratappandroid/Registration.java
@@ -1,10 +1,38 @@
 package com.example.paramount.ratappandroid;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
- * Created by joshuareno on 9/21/17.
+ * Registration screen with cancel and submit buttons.
+ * Greg Johnston 9/21/2017
  */
 
 public class Registration extends AppCompatActivity {
+    private Button cancel;
+    private Button submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration);
+
+        cancel = (Button) findViewById(R.id.cancelButton);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        submit = (Button) findViewById(R.id.submitButton);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: handle submission here
+            }
+        });
+    }
 }
