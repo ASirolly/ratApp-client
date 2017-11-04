c75329aa267de5be98f45a573a151b53e660fa61
jreno3 <joshuagregr@gmail.com>
2017-11-04 14:44:43 -0400

    added graph class

---
+++ app/src/main/java/com/example/paramount/ratappandroid/GraphActivity.java
@@ -0,0 +1,11 @@
+package com.example.paramount.ratappandroid;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by joshuareno on 11/4/17.
+ */
+
+public class GraphActivity extends AppCompatActivity {
+    
+}
