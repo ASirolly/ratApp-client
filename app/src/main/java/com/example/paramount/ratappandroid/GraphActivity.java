3cd4440fd9cf047e815fd9ad51b8ec5ef7538562
jreno3 <joshuagregr@gmail.com>
2017-11-04 14:48:42 -0400

    added graph class again

--- app/src/main/java/com/example/paramount/ratappandroid/GraphActivity.java
+++ app/src/main/java/com/example/paramount/ratappandroid/GraphActivity.java
@@ -1,11 +1,24 @@
 package com.example.paramount.ratappandroid;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import java.util.Date;
 
 /**
  * Created by joshuareno on 11/4/17.
  */
 
 public class GraphActivity extends AppCompatActivity {
-    
+    private static final String selectStartDateButtonTextTemplate =
+            "SELECT START DATE (selected date: %s)";
+    private static final String selectEndDateButtonTextTemplate =
+            "SELECT END DATE (selected date: %s)";
+    private Button selectStartDateButton;
+    private Button selectEndDateButton;
+    private DatePickerDialog startDatePickerDialog;
+    private DatePickerDialog endDatePickerDialog;
+    private Date startDate;
+    private Date endDate;
 }
