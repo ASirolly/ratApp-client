fc90130770f926d009f3337606655d74d827f88b
jreno3 <joshuagregr@gmail.com>
2017-11-07 11:34:36 -0500

    graph finished

--- app/src/main/java/com/example/paramount/ratappandroid/GraphActivity.java
+++ app/src/main/java/com/example/paramount/ratappandroid/GraphActivity.java
@@ -4,10 +4,14 @@ import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.SyncStatusObserver;
 import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.RelativeLayout;
 
 import com.example.paramount.ratappandroid.dao.GraphDateDAO;
@@ -26,6 +30,11 @@ import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.example.paramount.ratappandroid.DayAxisValueFormatter;
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.LegendRenderer;
+import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
@@ -66,12 +75,29 @@ public class GraphActivity extends AppCompatActivity {
     private Date startDate;
     private Date endDate;
 
-    private LineChart lineChart;
-    private RelativeLayout rl; // need to change
-    private List<Entry> entriesList = new ArrayList<Entry>();
-    private HashMap<Date, Integer> frequency = new HashMap<>();
-    private BarChart barChart;
+//    private LineChart lineChart;
+//    private RelativeLayout rl; // need to change
+//    private List<Entry> entriesList = new ArrayList<Entry>();
+//    private HashMap<Date, Integer> frequency = new HashMap<>();
+//    private BarChart barChart;
 
+    private DataPoint[] ten = new DataPoint[12];
+    private DataPoint[] eleven = new DataPoint[12];
+    private DataPoint[] twelve = new DataPoint[12];
+    private DataPoint[] thirteen = new DataPoint[12];
+    private DataPoint[] fourteen = new DataPoint[12];
+    private DataPoint[] fifteen = new DataPoint[12];
+    private DataPoint[] sixteen = new DataPoint[12];
+    private DataPoint[] seventeen = new DataPoint[12];
+    private LineGraphSeries<DataPoint> series2010;
+    private LineGraphSeries<DataPoint> series2011;
+    private LineGraphSeries<DataPoint> series2012;
+    private LineGraphSeries<DataPoint> series2013;
+    private LineGraphSeries<DataPoint> series2014;
+    private LineGraphSeries<DataPoint> series2015;
+    private LineGraphSeries<DataPoint> series2016;
+    private LineGraphSeries<DataPoint> series2017;
+    private GraphView graphChart;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -153,33 +179,6 @@ public class GraphActivity extends AppCompatActivity {
         System.out.println(temp.get(0));
     }
 
-//    private void showAllFrequencies() {
-//        Log.d("this", "tag7");
-//        ArrayList<GraphDate> temp = Model.getInstance().getGraphDates();
-//        List<BarEntry> barData = new LinkedList<>();
-//        List<String> xValuesDates = new LinkedList<>();
-//        System.out.println("THIS IS SIZE OF temp: " + temp.size());
-//        for (GraphDate gD : temp) {
-//            String dat = gD.getMonth() + "-" + gD.getYear();
-//            int month = Integer.parseInt(gD.getMonth());
-//            int frequency = Integer.parseInt(gD.getFrequency());
-//            xValuesDates.add(dat);
-//            barData.add(new BarEntry(month, frequency));
-//        }
-//        System.out.println("PRINGINT OUT STUFFFFF");
-//        for (BarEntry bE: barData) {
-//            System.out.println(bE);
-//        }
-//
-//        System.out.println("All fine");
-//
-//        XAxis xAxis = barChart.getXAxis();
-//        xAxis.setValueFormatter(new DayAxisValueFormatter(barChart));
-//        BarDataSet set = new BarDataSet(barData, "entries");
-//        BarData barDat = new BarData(set);
-//        barChart.setData(barDat);
-//        barChart.invalidate();
-//    }
     private void showAllFrequencies() {
         ArrayList<GraphDate> temp = Model.getInstance().getGraphDates();
         LineGraphSeries<DataPoint> series;
@@ -197,25 +196,283 @@ public class GraphActivity extends AppCompatActivity {
                 date.setMonth(month);
                 int frequency = Integer.parseInt(gD.getFrequency());
                 dataPoints[count] = new DataPoint(date.getMonth(), frequency);
+                Log.d("thing", "a" + year + "c");
+                switch(year) {
+                    case 2010:
+                        Log.d("ten", "");
+                        Log.d("ten", "" + month);
+                        try {
+                            Log.d("ten", "");
+                            ten[month] = new DataPoint(date.getMonth(), frequency);
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                            // do alert
+                        }
+                        break;
+                    case 2011:
+                        try {
+                            Log.d("eleven", "");
+                            eleven[month] = new DataPoint(date.getMonth(), frequency);
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                            // do alert
+                        }
+                        break;
+                    case 2012:
+                        try {
+                            Log.d("twelve", "");
+                            twelve[month] = new DataPoint(date.getMonth(), frequency);
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                            // do alert
+                        }
+                        break;
+                    case 2013:
+                        try {
+                            Log.d("thirteen", "");
+                            thirteen[month] = new DataPoint(date.getMonth(), frequency);
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                            // do alert
+                        }
+                        break;
+                    case 2014:
+                        try {
+                            Log.d("fourteen", "");
+                            fourteen[month] = new DataPoint(date.getMonth(), frequency);
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                            // do alert
+                        }
+                        break;
+                    case 2015:
+                        try {
+                            Log.d("fifteen", "");
+                            fifteen[month] = new DataPoint(date.getMonth(), frequency);
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                            // do alert
+                        }
+                        break;
+                    case 2016:
+                        Log.d("six", "");
+                        Log.d("six", "" + month);
+                        try {
+                            Log.d("sixteen", "");
+                            sixteen[month] = new DataPoint(date.getMonth(), frequency);
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                            // do alert
+                        }
+                        break;
+                    case 2017:
+                        Log.d("seven", "abc");
+                        Log.d("seven", "" + month);
+                        try {
+                            Log.d("seventeen", "hi");
+                            seventeen[month] = new DataPoint(date.getMonth(), frequency);
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                            Log.d("seventeen", "asdfsadfasdfasdf");
+                        }
+                        break;
+                }
                 count++;
             }
         }
-        series = new LineGraphSeries<DataPoint>(dataPoints);
-        GraphView graph = (GraphView) findViewById(R.id.chart);
+        populate();
+        series2010 = new LineGraphSeries<>(ten);
+        series2011 = new LineGraphSeries<>(eleven);
+        series2012 = new LineGraphSeries<>(twelve);
+        series2013 = new LineGraphSeries<>(thirteen);
+        series2014 = new LineGraphSeries<>(fourteen);
+        series2015 = new LineGraphSeries<>(fifteen);
+        series2016 = new LineGraphSeries<>(sixteen);
+        series2017 = new LineGraphSeries<>(seventeen);
 
-//        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-//                new DataPoint(0, 1),
-//                new DataPoint(1, 5),
-//                new DataPoint(2, 3),
-//                new DataPoint(3, 2),
-//                new DataPoint(4, 6)
-//        });
-        series.setTitle("This");
-        series.setColor(Color.GREEN);
-        series.setDrawDataPoints(true);
-        series.setDataPointsRadius(10);
-        series.setThickness(8);
+        //series = new LineGraphSeries<>(dataPoints);
 
-        graph.addSeries(series);
+        populateColor();
+        graphChart = (GraphView) findViewById(R.id.chart);
+
+        //GraphView graph = (GraphView) findViewById(R.id.chart);
+        //graph.removeAllSeries();
+        //graph.addSeries(series);
+        graphChart.removeAllSeries();
+        graphChart.addSeries(series2010);
+    }
+
+    public void populateColor() {
+        series2010.setTitle("This");
+        series2010.setColor(Color.GREEN);
+        series2010.setDrawDataPoints(true);
+        series2010.setDataPointsRadius(10);
+        series2010.setThickness(8);
+
+        series2011.setTitle("This");
+        series2011.setColor(Color.GREEN);
+        series2011.setDrawDataPoints(true);
+        series2011.setDataPointsRadius(10);
+        series2011.setThickness(8);
+
+        series2012.setTitle("This");
+        series2012.setColor(Color.GREEN);
+        series2012.setDrawDataPoints(true);
+        series2012.setDataPointsRadius(10);
+        series2012.setThickness(8);
+
+        series2013.setTitle("This");
+        series2013.setColor(Color.GREEN);
+        series2013.setDrawDataPoints(true);
+        series2013.setDataPointsRadius(10);
+        series2013.setThickness(8);
+
+        series2014.setTitle("This");
+        series2014.setColor(Color.GREEN);
+        series2014.setDrawDataPoints(true);
+        series2014.setDataPointsRadius(10);
+        series2014.setThickness(8);
+
+        series2015.setTitle("This");
+        series2015.setColor(Color.GREEN);
+        series2015.setDrawDataPoints(true);
+        series2015.setDataPointsRadius(10);
+        series2015.setThickness(8);
+
+        series2016.setTitle("This");
+        series2016.setColor(Color.GREEN);
+        series2016.setDrawDataPoints(true);
+        series2016.setDataPointsRadius(10);
+        series2016.setThickness(8);
+
+        series2017.setTitle("This");
+        series2017.setColor(Color.GREEN);
+        series2017.setDrawDataPoints(true);
+        series2017.setDataPointsRadius(10);
+        series2017.setThickness(8);
+    }
+
+    public void populate() {
+        for (int i = 0; i < ten.length; i++) {
+            if (ten[i] == null ) {
+                ten[i] = new DataPoint(i, 0);
+            } else if (ten[i].getY() != 0) {
+                ten[i] = new DataPoint(i, 0);
+            }
+        }
+        for (int i = 0; i < eleven.length; i++) {
+            if (eleven[i] == null) {
+                eleven[i] = new DataPoint(i, 0);
+            } else if (eleven[i].getY() == 0) {
+                eleven[i] = new DataPoint(i, 0);
+            }
+        }
+        for (int i = 0; i < twelve.length; i++) {
+            if (twelve[i] == null) {
+                twelve[i] = new DataPoint(i, 0);
+            } else if (twelve[i].getY() == 0) {
+                twelve[i] = new DataPoint(i, 0);
+            }
+        }
+        for (int i = 0; i < thirteen.length; i++) {
+            if (thirteen[i] == null) {
+                thirteen[i] = new DataPoint(i, 0);
+            } else if (thirteen[i].getY() == 0) {
+                thirteen[i] = new DataPoint(i, 0);
+            }
+        }
+        for (int i = 0; i < fourteen.length; i++) {
+            if (fourteen[i] == null) {
+                fourteen[i] = new DataPoint(i, 0);
+            } else if (fourteen[i].getY() == 0) {
+                fourteen[i] = new DataPoint(i, 0);
+            }
+        }
+        for (int i = 0; i < fifteen.length; i++) {
+            if (fifteen[i] == null) {
+                fifteen[i] = new DataPoint(i, 0);
+            } else if (fifteen[i].getY() == 0) {
+                fifteen[i] = new DataPoint(i, 0);
+            }
+        }
+        for (int i = 0; i < sixteen.length; i++) {
+            if (sixteen[i] == null) {
+                sixteen[i] = new DataPoint(i, 0);
+            } else if (sixteen[i].getY() == 0) {
+                sixteen[i] = new DataPoint(i, 0);
+            }
+        }
+        for (int i = 0; i < seventeen.length; i++) {
+            if (seventeen[i] == null) {
+                seventeen[i] = new DataPoint(i, 0);
+            } else if (seventeen[i].getY() == 0) {
+                seventeen[i] = new DataPoint(i, 0);
+            }
+        }
+    }
+
+    public void onSwitchYear(View view) {
+        boolean check = ((RadioButton) view).isChecked();
+        Log.d("" + view.getId(), "thing");
+        RadioButton button = (RadioButton) view;
+
+        switch(Integer.parseInt((String) button.getText())) {
+            case 2010:
+                if(check) {
+                    graphChart.removeAllSeries();
+                    if (series2010 != null) {
+                        graphChart.addSeries(series2010);
+                    }
+                }
+                break;
+            case 2011:
+                if (check) {
+                    graphChart.removeAllSeries();
+                    if (series2011 != null) {
+                        graphChart.addSeries(series2011);
+                    }
+                }
+                break;
+            case 2012:
+                if (check) {
+                    graphChart.removeAllSeries();
+                    if (series2012 != null) {
+                        graphChart.addSeries(series2012);
+                    }
+                }
+                break;
+            case 2013:
+                if (check) {
+                    graphChart.removeAllSeries();
+                    if (series2013 != null) {
+                        graphChart.addSeries(series2013);
+                    }
+                }
+                break;
+            case 2014:
+                if (check) {
+                    graphChart.removeAllSeries();
+                    if (series2014 != null) {
+                        graphChart.addSeries(series2014);
+                    }
+                }
+                break;
+            case 2015:
+                if (check) {
+                    graphChart.removeAllSeries();
+                    if (series2015 != null) {
+                        graphChart.addSeries(series2015);
+                    }
+                }
+                break;
+            case 2016:
+                if (check) {
+                    graphChart.removeAllSeries();
+                    if (series2016 != null) {
+                        graphChart.addSeries(series2016);
+                    }
+                }
+                break;
+            case 2017:
+                if (check) {
+                    graphChart.removeAllSeries();
+                    if (series2017 != null) {
+                        graphChart.addSeries(series2017);
+                    }
+                }
+                break;
+        }
     }
 }
