e6900ca680ef22e84aa3f4f8ee66e450e30f4147
jreno3 <joshuagregr@gmail.com>
2017-11-19 22:12:37 -0500

    removed more test problems

--- app/src/test/java/com/example/paramount/ratappandroid/DashboardTest.java
+++ app/src/test/java/com/example/paramount/ratappandroid/DashboardTest.java
@@ -1,9 +1,9 @@
 package com.example.paramount.ratappandroid;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.robolectric.RobolectricTestRunner;
-
-
 import static org.junit.Assert.assertEquals;
 
 /**
